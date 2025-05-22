09a290a62035efbc0ea4d572cbd60ad2498834c3
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-05-22 12:00:15 +0700

    Input langsung, untuk mengecek umur perizinan penggunaan ktp

---
+++ InputLangsung.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int umur = 17;
+
+    if (umur >= 17){
+        cout << "Kamu sudah bisa memiliki KTP" << endl;
+    }
+    else {
+        cout << "Kamu belum bisa memiliki KTP" << endl;
+    }
+    return 0;
+}
